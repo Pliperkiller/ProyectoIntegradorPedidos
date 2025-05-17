2225fdb0130cf97ec435ea9861ddadbdf0c0b5a4
pliperkiller <pip.kro@gmail.com>
2025-05-17 00:12:02 -0500

    Agegados datos semilla para inventario en InventarioMicroService\src\main\resources\data.sql

--- InventarioMicroService/src/main/resources/data.sql
+++ InventarioMicroService/src/main/resources/data.sql
@@ -1,25 +1,39 @@
 -- Insertar ingredientes
-INSERT INTO ingredientes (nombre, stock) VALUES ('Cheese', 100);
-INSERT INTO ingredientes (nombre, stock) VALUES ('Tomato', 80);
-INSERT INTO ingredientes (nombre, stock) VALUES ('Flour', 200);
-INSERT INTO ingredientes (nombre, stock) VALUES ('Meat', 50);
-INSERT INTO ingredientes (nombre, stock) VALUES ('Bread', 200);
+INSERT INTO ingredientes (nombre, stock) VALUES ('Bread', 200); -- Pan
+INSERT INTO ingredientes (nombre, stock) VALUES ('Potato', 300); -- Papa
+INSERT INTO ingredientes (nombre, stock) VALUES ('Sausage', 150); -- Salchicha
+INSERT INTO ingredientes (nombre, stock) VALUES ('Pasta', 100); -- Pasta
+INSERT INTO ingredientes (nombre, stock) VALUES ('Corn', 250); -- Maíz
+INSERT INTO ingredientes (nombre, stock) VALUES ('Meat', 100); -- Carne
 
 -- Insertar productos
-INSERT INTO productos (nombre) VALUES ('Pizza Margherita');
-INSERT INTO productos (nombre) VALUES ('Burger');
+INSERT INTO productos (nombre) VALUES ('Burguer');
+INSERT INTO productos (nombre) VALUES ('Fries');
+INSERT INTO productos (nombre) VALUES ('Hot dog');
+INSERT INTO productos (nombre) VALUES ('Spaggethy');
+INSERT INTO productos (nombre) VALUES ('Corn');
 
--- -- Insertar recetas
-INSERT INTO recetas (id_producto) VALUES (1);
-INSERT INTO recetas (id_producto) VALUES (2);
+-- Insertar recetas
+INSERT INTO recetas (id_producto) VALUES (1); -- Receta para Burguer
+INSERT INTO recetas (id_producto) VALUES (2); -- Receta para Fries
+INSERT INTO recetas (id_producto) VALUES (3); -- Receta para Hot dog
+INSERT INTO recetas (id_producto) VALUES (4); -- Receta para Spaggethy
+INSERT INTO recetas (id_producto) VALUES (5); -- Receta para Corn
 
--- -- Insertar ingredientes de recetas
--- -- Para Pizza Margherita
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (1, 1, 3); -- 3 unidades de queso
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (1, 2, 2); -- 2 unidades de tomate
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (1, 3, 5); -- 5 unidades de harina
+-- Insertar ingredientes de recetas
+-- Para Burguer
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (1, 1, 1); -- 1 unidad de pan
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (1, 6, 2); -- 2 unidades de carne
 
--- -- Para Burger
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (2, 1, 1); -- 1 unidad de queso
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (2, 2, 1); -- 1 unidad de tomate
-INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (2, 4, 2); -- 2 unidades de carne
+-- Para Fries
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (2, 2, 3); -- 3 unidades de papa
+
+-- Para Hot dog
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (3, 1, 1); -- 1 unidad de pan
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (3, 3, 1); -- 1 unidad de salchicha
+
+-- Para Spaggethy
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (4, 4, 1); -- 1 unidad de pasta
+
+-- Para Corn
+INSERT INTO recetas_ingredientes (id_receta, id_ingrediente, cantidad) VALUES (5, 5, 1); -- 1 unidad de maíz
